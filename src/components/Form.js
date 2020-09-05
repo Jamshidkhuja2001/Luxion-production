ebf82c0aa1bf5887876f100c92d7fd15a2f5bacb
Jamshidkhuja <jamshidxojab@gmail.com>
2020-09-05 19:01:06 +0500

    adaptive form

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,6 +15,7 @@ export const Form = (props) => {
   const [types, setTypes] = useState("");
   const [purchase, setPurchase] = useState("");
   const [sale, setSale] = useState("");
+  const [open, setOpen] = useState(false);
 
   const [selectedCategory, setSelectedCategory] = useState("");
   let Option = () => <ochishenie />;
@@ -24,7 +25,10 @@ export const Form = (props) => {
   if (selectedCategory === "Гигиеник") Option = () => <Gigienik />;
   if (selectedCategory === "Набор") Option = () => <Nabor />;
   if (selectedCategory === "Парфюм") Option = () => <Parfyum />;
-
+  const openHandler = (e) => {
+    e.preventDefault();
+    setOpen(true);
+  };
   const handleSubmit = (e) => {
     const product = {
       category,
@@ -43,85 +47,91 @@ export const Form = (props) => {
     console.log(product);
   };
   return (
-    <form
-      onSubmit={handleSubmit}
-      className=" form-group d-flex justify-content-between form-header pb-4"
-    >
-      <div className="d-flex flex-column">
-        <label htmlFor="cars">Категория</label>
-        <select
-          name=""
-          id=""
-          className="selector"
-          onChange={(e) => {
-            console.log(e.target.value);
-            setSelectedCategory(e.target.value);
-            return setCategory(e.target.value) && props.state(e.target.value);
-          }}
-          value={category}
-        >
-          <option value="">Выберите</option>
-          <option value="Очищение">Очищение</option>
-          <option value="Уход">Уход</option>
-          <option value="Декоратив">Декоратив</option>
-          <option value="Гигиеник">Гигиеник</option>
-          <option value="Набор">Набор</option>
-          <option value="Парфюм">Парфюм</option>
-        </select>
-      </div>
-      <div className="d-flex flex-column">
-        <label htmlFor="cars">Суб-Категория</label>
-        <select
-          name=""
-          id=""
-          className="selector"
-          onChange={(e) => setSubCategory(e.target.value)}
-          value={subCategory}
-        >
-          <Option />
-        </select>
-      </div>
-      <div className="d-flex flex-column">
-        <label htmlFor="cars">Имя Продукта</label>
-        <input
-          required
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-          value={name}
-        />
-      </div>
-      <div className="d-flex flex-column">
-        <label htmlFor="cars">Тип Продукта</label>
-        <input
-          required
-          type="text"
-          onChange={(e) => setTypes(e.target.value)}
-          value={types}
-        />
-      </div>
-      <div className="d-flex flex-column">
-        <label htmlFor="cars">Покупаемая Цена </label>
-        <input
-          required
-          type="number"
-          onChange={(e) => setPurchase(e.target.value)}
-          value={purchase}
-        />
-      </div>
-      <div className="d-flex flex-column">
-        <label htmlFor="cars">Продаваемая Цена </label>
-        <input
-          required
-          type="number"
-          onChange={(e) => setSale(e.target.value)}
-          value={sale}
-        />
-      </div>
-      <div className="d-flex flex-column mt-4">
-        <button type="submit" className="btn btn-primary">
-          Add
-        </button>
-      </div>
-    </form>
+    <>
+      <button className="btn btn-primary" onClick={openHandler}>
+        Добавить
+      </button>
+      {open ? (
+        <form onSubmit={handleSubmit} className="form-edit">
+          <div className="d-flex flex-column">
+            <label htmlFor="cars">Категория</label>
+            <select
+              name=""
+              id=""
+              className="selector"
+              onChange={(e) => {
+                console.log(e.target.value);
+                setSelectedCategory(e.target.value);
+                return (
+                  setCategory(e.target.value) && props.state(e.target.value)
+                );
+              }}
+              value={category}
+            >
+              <option value="">Выберите</option>
+              <option value="Очищение">Очищение</option>
+              <option value="Уход">Уход</option>
+              <option value="Декоратив">Декоратив</option>
+              <option value="Гигиеник">Гигиеник</option>
+              <option value="Набор">Набор</option>
+              <option value="Парфюм">Парфюм</option>
+            </select>
+          </div>
+          <div className="d-flex flex-column">
+            <label htmlFor="cars">Суб-Категория</label>
+            <select
+              name=""
+              id=""
+              className="selector"
+              onChange={(e) => setSubCategory(e.target.value)}
+              value={subCategory}
+            >
+              <Option />
+            </select>
+          </div>
+          <div className="d-flex flex-column">
+            <label htmlFor="cars">Имя Продукта</label>
+            <input
+              required
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              value={name}
+            />
+          </div>
+          <div className="d-flex flex-column">
+            <label htmlFor="cars">Тип Продукта</label>
+            <input
+              required
+              type="text"
+              onChange={(e) => setTypes(e.target.value)}
+              value={types}
+            />
+          </div>
+          <div className="d-flex flex-column">
+            <label htmlFor="cars">Покупаемая Цена </label>
+            <input
+              required
+              type="number"
+              onChange={(e) => setPurchase(e.target.value)}
+              value={purchase}
+            />
+          </div>
+          <div className="d-flex flex-column">
+            <label htmlFor="cars">Продаваемая Цена </label>
+            <input
+              required
+              type="number"
+              onChange={(e) => setSale(e.target.value)}
+              value={sale}
+            />
+          </div>
+          <div className="d-flex flex-column mt-4">
+            <button type="submit" className="btn btn-primary">
+              Add
+            </button>
+          </div>
+        </form>
+      ) : null}
+    </>
   );
 };
